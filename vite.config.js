3a3a0490d140e64f66b55b6b77e0db6f965b92a1
wd115 <wd115@students.waikato.ac.nz>
2025-04-20 19:33:09 +1200

    add

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/Top_100_Songs/',
   plugins: [react()],
 })
