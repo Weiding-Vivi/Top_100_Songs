1799060ad4cf2c068779c2a6d08a711756d696aa
wd115 <wd115@students.waikato.ac.nz>
2025-05-03 11:23:25 +1200

    dist

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: '/top100-music-visual/',  // 注意前后有斜杠
+
+  plugins: [react()]
 })
