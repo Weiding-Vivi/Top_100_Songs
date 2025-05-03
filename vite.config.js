c4f57992d35e519e505eb0eee2c00498bfdb7078
Weiding-Vivi <ding_wei_@outlook.com>
2025-05-03 13:41:43 +1200

    add more functions

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Top_100_Songs/',  // 你的 GitHub 仓库名，注意大小写要完全一致
+  base: '/Top_100_Songs/',  
   plugins: [react()]
 })
